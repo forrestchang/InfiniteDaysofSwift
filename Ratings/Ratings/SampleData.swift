5259cf49b2a388657bc857766bae94238436dec5
Jiayuan Zhang <forrestchang7@gmail.com>
2016-04-05 19:20:55 +0800

    finish the first project: Ratings

---
+++ Ratings/Ratings/SampleData.swift
@@ -0,0 +1,18 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by 张佳圆 on 4/5/16.
+//  Copyright © 2016 Tisoga. All rights reserved.
+//
+
+import Foundation
+
+let playersData = [
+    Player(name: "Jack", game: "Pokemon", rating: 4),
+    Player(name: "Mike", game: "GTA", rating: 5),
+    Player(name: "Nancy", game: "The Binding of Issac", rating: 3),
+    Player(name: "Tisoga", game: "轩辕剑", rating: 3)
+]
+
+
