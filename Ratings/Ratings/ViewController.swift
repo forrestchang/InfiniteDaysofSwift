464d737560ef6ec8ff1cb7c681e40ef82af5c36b
Jiayuan Zhang <forrestchang7@gmail.com>
2016-04-05 16:05:43 +0800

    init repo

---
+++ Ratings/Ratings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by 张佳圆 on 4/5/16.
+//  Copyright © 2016 Tisoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
